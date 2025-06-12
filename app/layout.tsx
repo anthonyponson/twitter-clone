3babe4dd09fe82077dd4fe0f87944ced358bd8bb
anthonyponson <antonyponson@gmail.com>
2025-06-12 20:10:36 +0530

    twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 
 import "./globals.css"
-import Sidebar from "@components/Sidebar"
 
 export default function RootLayout({
   children,
@@ -13,11 +12,11 @@ export default function RootLayout({
         <title>Twitter Clone</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="bg-black">
+      <body className="bg-gray-950 text-white font-sans antialiased">
         <div className="h-screen ">
           <div className="container h-full max-w-6xl mx-auto xl:px-30">
             <div className="grid grid-cols-4 h-full">
-              <Sidebar />
+             
 
               <div className="col-span-3 lg:col-span-2 hidden lg:block border-l border-gray-800">
                 {children}
