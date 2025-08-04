da67b1ef45a8fa532cc6db1aecc1ac642b0801bb
anthonyponson <antonyponson@gmail.com>
2025-08-04 22:38:49 +0530

    twitter

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-// src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from 'next-auth';
-import { AuthOptions } from 'next-auth';
-
-const handler = NextAuth(AuthOptions);
-
-export { handler as GET, handler as POST };
