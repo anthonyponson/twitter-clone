86fc939994b635bedf9e653898616b08a50bf3f5
anthonyponson <antonyponson@gmail.com>
2025-08-22 21:53:49 +0530

    twitter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from '@app/lib/Mongodb';
+import clientPromise from '@app/lib/mongodb';
 import dbConnect from '@app/lib/mongoose';
 import User from '@app/model/User';
 import bcrypt from 'bcryptjs';
