9b44e5ffb7585f71bbc13f65c01182b4c880a870
anthonyponson <antonyponson@gmail.com>
2026-01-15 23:20:16 +0530

    delete add function added

--- app/api/posts/[postId]/route.ts
+++ app/api/posts/[postId]/route.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next'; 
-import { authOptions } from '../../../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/[...nextauth]/route';
 import dbConnect from '@app/lib/mongoose';
 import Post from '@app/model/Post'; // Using your corrected path
 import User from '@app/model/User'; // Import User for population
