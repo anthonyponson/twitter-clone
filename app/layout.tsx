f68a5dfa953e615ed07a47585069f8ab5dd88dd9
anthonyponson <antonyponson@gmail.com>
2026-07-03 20:50:03 +0530

    style css

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from '@/components/Sidebar';
 import Widgets from '@/components/Widgets';
 import AuthProvider from '@/components/AuthProvider';
 import { ConfirmationDialog } from '@/components/ConfirmationDialog'; // <-- IMPORT
-
+import './globals.css'
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = { /* ... */ };
